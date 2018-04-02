047663971d39890d2e8f681cb1708a642fe1472a
mahend101 <mahend101@protonmail.com>
2018-04-02 13:15:51 +0200

    test project

---
+++ Test/ConsoleApplication11/ConsoleApplication11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using AForge.Video.FFMPEG;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+
+           Console.WriteLine("done");
+           Console.Read();
+        }
+
+        private createVideo()
+        {
+            int width = 320;
+            int height = 240;
+
+            //// create instance of video writer
+            VideoFileWriter writer = new VideoFileWriter();
+            //// create new video file
+            writer.Open("test.avi", width, height, 25, VideoCodec.MPEG4);
+            // create a bitmap to save into the video file
+            Bitmap image = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            // write 1000 video frames
+            for (int i = 0; i < 1000; i++)
+            {
+                image.SetPixel(i % width, i % height, Color.Red);
+                writer.WriteVideoFrame(image);
+            }
+            writer.Close();
+        }
+    }
+
+
+}
